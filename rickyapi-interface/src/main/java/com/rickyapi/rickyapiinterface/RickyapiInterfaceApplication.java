7c510cde56d45faaeee5eccef51ac0238d5b4665
ricky <592961269@qq.com>
2024-04-13 17:51:57 +0800

    你的名字自定义小接口

---
+++ rickyapi-interface/src/main/java/com/rickyapi/rickyapiinterface/RickyapiInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.rickyapi.rickyapiinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickyapiInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RickyapiInterfaceApplication.class, args);
+    }
+
+}
